39563fcbe0f914e2429c20a5282ebc5b44ed7bff
zhuweijin <472214158@qq.com>
2017-07-17 18:41:36 +0800

    新增着色器例子

--- opengl02/opengl02/opengl02.cpp
+++ opengl02/opengl02/opengl02.cpp
@@ -2,10 +2,154 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+/* 顶点着色器 */
+const char *vertexShaderSrc = "#version 330 core\n"
+"layout (location=0) int vec3 pos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
+"}\n";
+
+/* 编译着色器 */
+const char *fragShaderSrc = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main()
 {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "MyOpenGL02", nullptr, nullptr);
+	if (!window)
+	{
+		cout << "glfw create window fail\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+#if 0
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "fail to inti glad\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	int		ret;
+	char	error[256];
+	/* 编译 VetexShader */
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSrc, nullptr);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &ret);
+	if (!ret)
+	{
+		glGetShaderInfoLog(vertexShader, 256, nullptr, error);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << error << endl;
+	}
+
+	int fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragShader, 1, &fragShaderSrc, nullptr);
+	glCompileShader(fragShader);
+
+	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &ret);
+	if (!ret)
+	{
+		memset(error, 0, sizeof(error));
+		glGetShaderInfoLog(vertexShader, 256, nullptr, error);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << error << endl;
+	}
+
+	/* 链接着色器 */
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &ret);
+	if (ret)
+	{
+		glGetShaderInfoLog(vertexShader, 256, nullptr, error);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << error << endl;
+	}
+
+	/* 删除着色器 */
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragShader);
+
+	float vertexs[] = {
+		-0.5f, -0.5f, 0.0f, // 左
+		0.5f, -0.5f, 0.0f,	// 右 
+		0.0f,  0.5f, 0.0f	// 前 
+	};
+
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexs), vertexs, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+#endif
+	glfwTerminate();
+
     return 0;
 }
 
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
