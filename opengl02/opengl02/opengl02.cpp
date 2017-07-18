e470e97de21223f601995f4fded62fdc6966509d
zhuweijin <472214158@qq.com>
2017-07-18 19:53:18 +0800

    update

--- opengl02/opengl02/opengl02.cpp
+++ opengl02/opengl02/opengl02.cpp
@@ -17,7 +17,7 @@ const char *vertexShaderSrc =  "#version 330 core\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 	"}\0";
 
-/* 编译着色器 */
+/* 片段着色器 */
 const char *fragShaderSrc = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
@@ -67,7 +67,7 @@ int main()
 		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << error << endl;
 	}
 
-	/* */
+	/* 编译片段着色器 */
 	int fragShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragShader, 1, &fragShaderSrc, nullptr);
 	glCompileShader(fragShader);
@@ -104,12 +104,14 @@ int main()
 	};
 
 	unsigned int VAO, VBO;
+
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
+	/* 绑定 */
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexs), vertexs, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -127,6 +129,7 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// 激活
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 		glDrawArrays(GL_TRIANGLES, 0, 3);
